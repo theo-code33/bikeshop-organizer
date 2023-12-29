0a46e89f7393f5316e3adc459aede2db5c483476
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-12-29 16:27:42 +0100

    Generate Nest application

---
+++ apps/bikeshop-organizer-api/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'bikeshop-organizer-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/bikeshop-organizer-api',
+};
