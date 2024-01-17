f54ebe18e13bec2c543f58dc1c8830a6bf04c580
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2024-01-17 21:01:47 +0100

    feat(api): create auth resource

--- apps/bikeshop-organizer-api/src/app/auth/auth.controller.ts
+++ apps/bikeshop-organizer-api/src/app/auth/auth.controller.ts
@@ -1,7 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpException, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UsersService
+  ) {}
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto) {
+    try {
+      const user = await this.userService.create(createUserDto);
+      const payload = {
+        email: user.email,
+      };
+      const token = await this.authService.signPayload(payload);
+      return { user, token };
+    } catch (error) {
+      throw new HttpException(error.message, error.status);
+    }
+  }
+
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    try {
+      const user = await this.userService.findForLogin(loginDto);
+      const payload = {
+        email: user.email,
+      };
+      const token = await this.authService.signPayload(payload);
+      return { user, token };
+    } catch (error) {
+      throw new HttpException(error.message, error.status);
+    }
+  }
 }
