116e557c361d3741e41fb5381efa01e4a58b6656
theo-code33 <gillettheo1@gmail.com>
2024-01-21 18:43:56 +0100

    refacto(api): rename users resource to user

--- apps/bikeshop-organizer-api/src/app/auth/auth.service.ts
+++ apps/bikeshop-organizer-api/src/app/auth/auth.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { UsersService } from '../users/users.service';
+import { UserService } from '../user/user.service';
 import { sign } from 'jsonwebtoken';
 import { Payload } from './types/payload.type';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(private userService: UserService) {}
 
   async signPayload(payload: Payload) {
     return sign(payload, process.env.SECRET_KEY, { expiresIn: '7d' });
