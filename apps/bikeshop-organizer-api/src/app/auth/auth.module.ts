e0f86e860f6144c1c34fc9ba65c51fc9e075503d
theo-code33 <gillettheo1@gmail.com>
2024-01-22 08:34:53 +0100

    wip(api): add import of Passport Module

--- apps/bikeshop-organizer-api/src/app/auth/auth.module.ts
+++ apps/bikeshop-organizer-api/src/app/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { JwtStrategy } from './strategy/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PassportModule],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
 })
