8327c23cc58e9fa6f94b6de9342757a075858cf6
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2024-01-23 21:05:47 +0100

    feat(api): secure update user

--- apps/bikeshop-organizer-api/src/app/user/user.controller.ts
+++ apps/bikeshop-organizer-api/src/app/user/user.controller.ts
@@ -6,6 +6,9 @@ import {
   Param,
   Delete,
   UseGuards,
+  Req,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -13,6 +16,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { Roles as RolesEnum } from '@bikeshop-organizer/types';
+import { User } from './entities/user.entity';
 
 @Controller('user')
 @UseGuards(AuthGuard('jwt'), RolesGuard)
@@ -21,17 +25,46 @@ export class UserController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.userService.findOne(id);
+    try {
+      return this.userService.findOne(id);
+    } catch (error) {
+      throw new HttpException(
+        error.message,
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(id, updateUserDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+    @Req() req: Request & { user: User }
+  ) {
+    try {
+      const { user } = req;
+      if (user.role === RolesEnum.ADMIN || user.id === id) {
+        return this.userService.update(id, updateUserDto);
+      }
+      throw new HttpException('Forbidden access', HttpStatus.FORBIDDEN);
+    } catch (error) {
+      throw new HttpException(
+        error.message,
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
   }
 
   @Delete(':id')
   @Roles(RolesEnum.ADMIN)
   remove(@Param('id') id: string) {
-    return this.userService.remove(id);
+    try {
+      return this.userService.remove(id);
+    } catch (error) {
+      throw new HttpException(
+        error.message,
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
   }
 }
