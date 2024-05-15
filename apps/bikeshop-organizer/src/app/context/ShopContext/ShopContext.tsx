97c775bdef1ba89f4f3c12ce87cd0af6c981b56c
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2024-05-15 11:09:50 +0200

    feat(front): create getShopById utils

--- apps/bikeshop-organizer/src/app/context/ShopContext/ShopContext.tsx
+++ apps/bikeshop-organizer/src/app/context/ShopContext/ShopContext.tsx
@@ -2,6 +2,7 @@ import { createContext, useContext, useEffect, useState } from 'react';
 import { ShopCtx } from './types';
 import { Shop } from '@bikeshop-organizer/types';
 import { useAuth } from '../AuthContext/AuthContext';
+import getShopById from '../../utils/api/shop/get-shop-by-id';
 
 const ShopContext = createContext<ShopCtx>({
   shop: null,
@@ -12,9 +13,13 @@ const ShopProvider = ({ children }: { children: JSX.Element }) => {
   const [shop, setShop] = useState<Shop | null>(null);
   const { user } = useAuth();
   useEffect(() => {
-    if (user && user.shop) {
-      setShop(user.shop);
-    }
+    (async () => {
+      if (user && user.shop && user.shop.id) {
+        const shopApi = await getShopById(user.shop.id);
+        setShop(shopApi);
+        console.log(shopApi);
+      }
+    })();
   }, [user]);
   return (
     <ShopContext.Provider value={{ shop: shop, setShop: setShop }}>
