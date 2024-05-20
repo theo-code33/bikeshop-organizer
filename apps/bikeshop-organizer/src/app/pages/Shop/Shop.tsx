4a13097aedc12cc262d4e619d11b48bb09e7f4db
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2024-05-20 19:14:49 +0200

    wip(front): create shop page

---
+++ apps/bikeshop-organizer/src/app/pages/Shop/Shop.tsx
@@ -0,0 +1,11 @@
+import MainLayout from '../../layout/Main/Main.layout';
+
+const Shop = () => {
+  return (
+    <MainLayout>
+      <h1>Shop</h1>
+    </MainLayout>
+  );
+};
+
+export default Shop;
