d43296fe69c89eb9c8809b303f9319958a48af32
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2024-06-24 18:37:14 +0200

    feat(api): add bikes brand relation

--- apps/bikeshop-organizer-api/src/app/shop/shop.service.ts
+++ apps/bikeshop-organizer-api/src/app/shop/shop.service.ts
@@ -24,6 +24,7 @@ export class ShopService {
         'user',
         'clients',
         'clients.bikes',
+        'clients.bikes.brand',
         'products',
         'products.brand',
         'products.category',
