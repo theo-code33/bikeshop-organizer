ef2e0194842676a762ff4f5cbab26e29561d08d5
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2024-07-02 16:47:46 +0200

    refactor(front): add good field

--- libs/types/src/types/task.type.ts
+++ libs/types/src/types/task.type.ts
@@ -1,14 +1,17 @@
 import { Bike } from './bike.type';
+import { Client } from './client.type';
 import { Common } from './common.type';
 import { Product } from './product.type';
 import { TaskCategory } from './taskCategory.type';
 import { TaskCategoryStatus } from './taskCategoryStatus.type';
 
 export interface Task extends Common {
-  products: Product[];
-  status: TaskCategoryStatus;
+  name: string;
+  products?: Product[];
+  taskCategoryStatus: TaskCategoryStatus;
   mission: TaskCategory;
   bike: Bike;
-  startDate: Date;
-  endDate: Date;
+  client: Client;
+  startDate: string;
+  endDate: string;
 }
