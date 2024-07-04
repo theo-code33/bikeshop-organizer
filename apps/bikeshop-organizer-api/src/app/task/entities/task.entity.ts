7757008c4fbeaddd223d0e85a11193d4a3e68e85
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2024-07-04 19:13:54 +0200

    wip(api): add name and client field

--- apps/bikeshop-organizer-api/src/app/task/entities/task.entity.ts
+++ apps/bikeshop-organizer-api/src/app/task/entities/task.entity.ts
@@ -13,11 +13,14 @@ import { TaskCategory } from '../../task-category/entities/task-category.entity'
 import { TaskCategoryStatus } from '../../task-category-status/entities/task-category-status.entity';
 import { Bike } from '../../bike/entities/bike.entity';
 import { Product } from '../../product/entities/product.entity';
+import { Client } from '../../client/entities/client.entity';
 
 @Entity()
 export class Task {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+  @Column()
+  name: string;
   @ManyToMany(() => Product)
   @JoinTable()
   products?: Product[];
@@ -28,6 +31,8 @@ export class Task {
     (taskCategoryStatus) => taskCategoryStatus.tasks
   )
   taskCategoryStatus: TaskCategoryStatus;
+  @ManyToOne(() => Client, (client) => client.tasks)
+  client: Client;
   @ManyToOne(() => Bike, (bike) => bike.tasks)
   bike: Bike;
   @Column({
