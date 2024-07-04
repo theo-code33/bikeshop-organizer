cc57a82cf9b44f7262a52453b88ece827fb0844d
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2024-07-04 19:19:06 +0200

    fix(api): adapt dto to entity

--- apps/bikeshop-organizer-api/src/app/task/dto/create-task.dto.ts
+++ apps/bikeshop-organizer-api/src/app/task/dto/create-task.dto.ts
@@ -2,11 +2,16 @@ import { DeepPartial } from 'typeorm';
 import { TaskCategory } from '../../task-category/entities/task-category.entity';
 import { TaskCategoryStatus } from '../../task-category-status/entities/task-category-status.entity';
 import { Bike } from '../../bike/entities/bike.entity';
+import { Client } from '../../client/entities/client.entity';
+import { Product } from '../../product/entities/product.entity';
 
 export class CreateTaskDto {
+  name: string;
   taskCategory: DeepPartial<TaskCategory>;
   taskCategoryStatus: DeepPartial<TaskCategoryStatus>;
   bike: DeepPartial<Bike>;
+  client: DeepPartial<Client>;
+  products?: DeepPartial<Product>[];
   startDate: string;
   endDate: string;
 }
